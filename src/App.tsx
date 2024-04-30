0007bbe42e0d43972f580a783efa6198dc5f6256
Semyon-Abramov <ab@mebel-vigo.ru>
2024-04-30 09:45:14 +0300

    работа с файлами xlsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,75 @@
-import { Box } from "@mui/material";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-extra-boolean-cast */
+import { Button, Stack, TextField, Typography } from "@mui/material";
 import "./App.css";
-import Node from "./components/Graphs/Node/Node";
+import * as XLSX from "xlsx";
+import { useRef, useState } from "react";
+
+interface IData {
+  Name: string;
+}
 
 function App() {
+  const filePicker = useRef<HTMLInputElement>(null);
+  const [selectedFiles, setSelectedFiles] = useState<Array<File>>([]);
+  const [data, setData] = useState<IData[]>([]);
+  const [inputValue, setInputValue] = useState("");
+
+  function inputChangeHandler(e: React.ChangeEvent) {
+    if (
+      e.target instanceof HTMLInputElement &&
+      e.target.type === "file" &&
+      e.target.files
+    ) {
+      setSelectedFiles(Array.prototype.slice.call(e.target.files));
+
+      const reader = new FileReader();
+      reader.readAsBinaryString(e.target.files[0]);
+      reader.onload = () => {
+        const workbook = XLSX.read(reader.result, { type: "binary" });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const parseData = XLSX.utils.sheet_to_json(sheet);
+        console.log(parseData);
+        setData(parseData as IData[]);
+      };
+    }
+  }
+
+  function handlePick() {
+    filePicker.current?.click();
+  }
+
   return (
-    <Box>
-      <Node />
-    </Box>
+    <Stack gap="15px" direction="row" alignItems="center">
+      <Button
+        variant="contained"
+        onClick={handlePick}
+        color={!Boolean(data.length) ? "primary" : "secondary"}
+      >
+        {!Boolean(data.length) ? "Загрузить файл" : "Файл загружен"}
+      </Button>
+      <input
+        type="file"
+        onChange={(e) => inputChangeHandler(e)}
+        ref={filePicker}
+        hidden
+      />
+      <TextField
+        placeholder={
+          !Boolean(data.length) ? "Загрузите файл" : "Что вы хотите найти?"
+        }
+        size="small"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        disabled={!Boolean(data.length)}
+      />
+      {data && inputValue && (
+        <Typography>
+          {data.find((row) => row.Name === inputValue) ? "Найден" : "Не найден"}
+        </Typography>
+      )}
+    </Stack>
   );
 }
 
--- src/components/Graphs/Node/Node.tsx
+++ src/components/Graphs/Node/Node.tsx
@@ -1,5 +1,20 @@
-import { NodeContainer } from "./styled";
+// import "./styles.css";
+import { useState } from "react";
+import { Reorder } from "framer-motion";
+import { Box } from "@mui/material";
 
-const Node = () => <NodeContainer>Text lorem ipsum!</NodeContainer>;
 
-export default Node;
+const initialItems = [{ name: 'Стандартный', values: [] },{ name: 'new', values: [] }];
+
+export default function Node() {
+  const [items, setItems] = useState(initialItems);
+
+  return (
+    <Reorder.Group axis="y" onReorder={setItems} values={items}>
+      {items.map((item) => (
+        <Reorder.Item key={item.name} value={item}><Box sx={{backgroundColor:'#fff', width:'200px',height:'50px' }}>{item.name}</Box> </Reorder.Item>
+      ))} 
+    </Reorder.Group>
+  );
+}
+
